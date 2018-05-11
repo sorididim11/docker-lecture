70bfd53b64c809c094df9912293ad10e7dd82962
sorididim11 <sorididim11@gmail.com>
2018-05-12 00:38:45 +0900

    add nodejs sample app

---
+++ nodejs/chatservice/app.js
@@ -0,0 +1,64 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var stylus = require('stylus');
+var MongoClient = require('mongodb').MongoClient;
+var Server = require('mongodb').Server;
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('port', process.env.PORT || 8888);
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(stylus.middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req,res,next){
+  req.db = db;
+  next();
+});
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+mongoContainreName = 'mymongo'
+MongoClient.connect('mongodb://' + mongoContainreName + ':27017/school', function(error, db){
+  if(error) {
+      console.log(error);
+  } else {
+    app.listen(8888, function() {
+      console.log('chat service listening on port 8888')
+      console.log('connected to mongodb')
+    })
+  }
+});
+
+module.exports = app;
